e42976b0c8a50288ee31de0a6a95cec0eff4c486
Taylor Huston <taylor.a.huston@gmail.com>
2019-08-12 20:41:28 -0700

    Set default description

--- BehaviourDefaultDescription.groovy
+++ BehaviourDefaultDescription.groovy
@@ -2,8 +2,11 @@
 Add this code as the initializer for a Behaviour mapped to the project.*/
 
 def desc = getFieldById("description")
-
 def defaultValue = "Some default"
+
+if (getActionName() != "Create Issue") {
+    return // not the initial action, so don't set default values
+}
 
 //Set if Description is empty
 if (!underlyingIssue?.description) { 
